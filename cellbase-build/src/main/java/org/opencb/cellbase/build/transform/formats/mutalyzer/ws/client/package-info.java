1c15d5b57d63ebad0440c23b39de2661d517fc98
Pablo Arce <pablo.arce@bioinfomgp.org>
2014-12-01 15:50:32 +0100

    Added mutalyzer WS client

---
+++ cellbase-build/src/main/java/org/opencb/cellbase/build/transform/formats/mutalyzer/ws/client/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://mutalyzer.nl/2.0/services", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package org.opencb.cellbase.build.transform.formats.mutalyzer.ws.client;
